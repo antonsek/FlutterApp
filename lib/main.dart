3ce116de3dc67e14885b573a1200be5ae927eae6
antonsek <44634016+antonsek@users.noreply.github.com>
2021-03-21 12:03:59 +0600

    Create project

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,7 @@ class MyApp extends StatelessWidget {
           title: Text('Hello'),
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Card(
-                child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Введите текст'
-                        ),
-                      ),
-                    ),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                ),
-                onPressed: () { },
-                child: Text('Вызвать уведомление'),
-              )
-            ],
-          ),
-        ),
+        //body: 
       ),
     );
   }
