60cf9ba19af4e09be1cf7db789e2673bbd5a4126
antonsek <44634016+antonsek@users.noreply.github.com>
2021-03-21 13:20:45 +0600

    Add DateTime in Title

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is t he root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello'),
-          centerTitle: true,
+    return new MaterialApp(
+      title: 'Intl Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Intl Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateFormat dateFormat;
+  DateFormat timeFormat;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting();
+    dateFormat = new DateFormat.MMMEd('ru');
+    timeFormat = new DateFormat.Hms('ru');
+  }
+
+  void _refresh() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _refresh());
+    var dateTime = new DateTime.now();
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(timeFormat.format(dateTime)),
+            new Text(dateFormat.format(dateTime)),
+          ],
         ),
-        //body: 
+      ),
+      //body: 
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _refresh,
+        tooltip: 'Refresh',
+        child: new Icon(Icons.refresh),
       ),
     );
   }
